17612862b1732f4d01736d12f1516825d7af8604
TechkingsSupport <support@techkings.co.za>
2022-03-29 14:34:01 +0200

    add CRU operations for the employee model

--- iSmartcoApp/models.py
+++ iSmartcoApp/models.py
@@ -1,11 +1,35 @@
 from statistics import mode
 from tkinter import CASCADE
-
-from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
 
-#class User(AbstractUser):
+
+
+class Company(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    email = models.CharField(max_length=255, null=True, blank=True)
+    website = models.CharField(max_length=255, null=True, blank=True)
+    created_by = models.IntegerField(default=0, null=True, blank=True)
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    id = models.AutoField(primary_key=True)
+    employee_id_num = models.CharField(max_length=13, unique=True)
+    employee_name = models.CharField(max_length=50, null=True, blank=True)
+    employee_phone = models.CharField(max_length=10, null=True, blank=True)
+    employee_address = models.CharField(max_length=100, null=True, blank=True)
+    employee_designation = models.CharField(max_length=50, null=True, blank=True)
+    employee_joining_date = models.DateField(blank=True, null=True)
+    employee_leaving_date = models.DateField(blank=True, null=True)
+    employee_company = models.CharField(max_length=100, null=True, blank=True)
+    
+    def __str__(self):
+        return self.employee_name
 
 class JobCard(models.Model):
     id = models.AutoField(primary_key=True)
@@ -27,6 +51,9 @@ class JobCard(models.Model):
     job_card_resolution = models.TextField(null=True, blank=True)
     job_card_completion_description = models.CharField(max_length=100, null=True, blank=True)
     job_card_nva_time = models.TimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.job_card_number
 
 
 '''
--- iSmartcoApp/serializers.py
+++ iSmartcoApp/serializers.py
@@ -1,49 +1,21 @@
 from rest_framework import serializers
 
-from iSmartcoApp.models import JobCard
+from iSmartcoApp.models import JobCard, Employee, Company
 
-'''
 class CompanySerializers(serializers.ModelSerializer):
     class Meta:
         model = Company
-        fields = ('companyId',
-                  'companyName',
-                  'streetAddress',
-                  'vatNum',
-                  'email',
-                  'createdBy')
+        fields = '__all__'
 
-class CustomUsersSerializers(serializers.ModelSerializer):
+
+class EmployeeSerializers(serializers.ModelSerializer):
     class Meta:
-        model = CustomUsers
-        fields = ('username',
-                  'first_name',
-                  'last_name',
-                  'email',
-                  'password',
-                  'user_type',
-                  'company')
-'''
+        model = Employee
+        fields = '__all__'
 
 
 class JobCardSerializers(serializers.ModelSerializer):
     class Meta:
         model = JobCard
-        fields = ('id',
-                  'job_card_number',
-                  'job_card_client',
-                  'job_card_requester',
-                  'job_card_reference',
-                  'job_card_location',
-                  'job_card_created_at',
-                  'job_card_started_at',
-                  'job_card_completed_at',
-                  'job_card_technician',
-                  'job_card_type',
-                  'job_card_status',
-                  'job_card_description',
-                  'job_card_priority',
-                  'job_card_resolution',
-                  'job_card_completion_description',
-                  'job_card_nva_time',)
+        fields = '__all__'
 
